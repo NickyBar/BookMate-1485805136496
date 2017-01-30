51eee49c3a617ecb9157d6ac5801098e80718f0e
NickyBar <nbarberis@yahoo.com>
2017-01-30 15:26:35 -0500

    Update BookMate.py

--- BookMate.py
+++ BookMate.py
@@ -1,10 +1,5 @@
-from __future__ import print_function
-try:
-    import curses
-    curses.setupterm()
-    assert curses.tigetnum("colors") > 2
-    OK = "\x1b[1;%dm[ OK ]\x1b[0m" % (30 + curses.COLOR_GREEN)
-    FAIL = "\x1b[1;%dm[FAIL]\x1b[0m" % (30 + curses.COLOR_RED)
-except:
-    OK = '[ OK ]'
-    FAIL = '[FAIL]'
+#from __future__ import print_function
+#try:
+    #import curses
+    #curses.setupterm()
+    
